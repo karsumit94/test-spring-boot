5d7320a27a2f0e8f1fb7f0d420791a9b2240ec34
shinjanidhar <shidhar@cisco.com>
2019-11-01 23:54:48 +0530

    second commit

---
+++ demo-webapp/src/test/java/com/dhar/demowebapp/DemoWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhar.demowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
